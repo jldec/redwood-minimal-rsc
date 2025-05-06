aa2a8098bb1afdbf7717fc0dcccae6b99344be90
jldec <jurgen@jldec.me>
2025-05-06 19:46:35 -0400

    tailwind

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -2,8 +2,8 @@ import { RequestInfo } from "rwsdk/worker";
 
 export function Home({ ctx }: RequestInfo) {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold">Hello World</h1>
     </div>
   );
 }
