21987d2f378c3dcfa5f4b72c1a1b71faeee9044f
jldec <jurgen@jldec.me>
2025-05-12 11:29:21 -0400

    clarify

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -6,7 +6,7 @@ import { ServerTime } from './ServerTime'
 export function Home() {
   return (
     <div className="flex flex-col items-center min-h-screen">
-      <h1 className="text-2xl font-bold my-2">RSC vs Client rendered</h1>
+      <h1 className="text-2xl font-bold my-2">RedwoodSDK RSC vs Client rendered</h1>
       <Clock />
       <ClientTimeButton />
       <ServerTimeButton />
