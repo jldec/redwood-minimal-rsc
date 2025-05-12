d6c6256b6505ef41e11e3413172348aa3e60a863
jldec <jurgen@jldec.me>
2025-05-12 13:29:23 -0400

    less magic explicit api route

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -7,9 +7,12 @@ export function Home() {
   return (
     <div className="flex flex-col items-center min-h-screen">
       <h1 className="text-2xl font-bold my-2">RedwoodSDK minimal RSC demo</h1>
-      <a href="https://github.com/jldec/redwood-minimal-rsc#readme" className="text-blue-600 underline mb-8">https://github.com/jldec/redwood-minimal-rsc</a>
+      <a href="https://github.com/jldec/redwood-minimal-rsc#readme" className="text-blue-600 underline mb-8">
+        https://github.com/jldec/redwood-minimal-rsc
+      </a>
       <Clock />
       <ClientTimeButton />
+      <ServerTimeButton callFetch />
       <ServerTimeButton />
       <ServerTime />
     </div>
