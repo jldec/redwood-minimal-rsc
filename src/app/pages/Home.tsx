c00a5aac53a1a7e2df43981c56eb9d6274ce52fa
jldec <jurgen@jldec.me>
2025-05-15 07:50:25 -0400

    font size

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -6,7 +6,7 @@ import { ServerTime } from './ServerTime'
 export function Home() {
   return (
     <div className="flex flex-col items-center min-h-screen text-sm">
-      <h1 className="text-2xl font-bold my-2">RedwoodSDK minimal RSC demo</h1>
+      <h1 className="text-xl font-bold my-2">RedwoodSDK minimal RSC demo</h1>
       <a href="https://github.com/jldec/redwood-minimal-rsc#readme" className="text-blue-600 p-2 underline mb-8 text-base">
         github.com/jldec/redwood-minimal-rsc
       </a>
