c73b5bfc3708d3ffedf98c82719db9a19d3e180b
jldec <jurgen@jldec.me>
2025-05-22 13:35:36 +0100

    fix Time header

--- src/app/time/Time.tsx
+++ src/app/time/Time.tsx
@@ -8,9 +8,9 @@ export function Time() {
   console.log('Home RSC')
   return (
     <div className="flex flex-col items-center min-h-screen text-sm">
-      <h1 className="text-xl font-bold my-2">Time</h1>
+      <h1 className="text-xl font-bold my-2">RedwoodSDK minimal RSC Demo</h1>
       <a href="/" className="text-blue-600 p-2 underline mb-8 text-base">
-        RedwoodSDK minimal RSC Demo
+        Home
       </a>
       <Clock />
       <ClientTimeButton />
