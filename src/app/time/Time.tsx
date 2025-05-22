98613cff5c28f62b7103c5f35e2aa70ed2c8fd19
jldec <jurgen@jldec.me>
2025-05-22 13:32:26 +0100

    fix package.json

--- src/app/time/Time.tsx
+++ src/app/time/Time.tsx
@@ -10,7 +10,7 @@ export function Time() {
     <div className="flex flex-col items-center min-h-screen text-sm">
       <h1 className="text-xl font-bold my-2">Time</h1>
       <a href="/" className="text-blue-600 p-2 underline mb-8 text-base">
-        Home
+        RedwoodSDK minimal RSC Demo
       </a>
       <Clock />
       <ClientTimeButton />
